d3c8012e9b5c86d33712addd7da1d1963246c2b4
Dave Kelsey <dmkelsey@gmail.com>
2014-10-05 21:41:06 -0700

    Update bvbdb.php

--- lib/bvbdb.php
+++ lib/bvbdb.php
@@ -1,8 +1,8 @@
 <?php
 
-define('BVB_DB_NAME', 'barker_remittance');
-define('BVB_DB_USER', 'root');
-define('BVB_DB_PASSWORD', '');
-define('BVB_DB_HOST', 'localhost');
+define('BVB_DB_NAME', '##################');
+define('BVB_DB_USER', '##########');
+define('BVB_DB_PASSWORD', '##########');
+define('BVB_DB_HOST', '###############');
 
 ?>
